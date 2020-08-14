b006008d2fcfb0f816c553f68e89d590d8fd9b1c
sunybee <suny_bee@163.com>
2020-08-14 22:43:25 +0800

    learnclass123

---
+++ learnclass123.py
@@ -0,0 +1,14 @@
+# 第一个类1234
+class Student:
+    name = "小明"
+    score = "90"
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+    def learn(self):
+        print("学生%s学习python" % self.name)
+    def exercise(self):
+        print("学生练习python得分%s" % self.score)
+s=Student("小明","90")
+s.learn()
+s.exercise()
