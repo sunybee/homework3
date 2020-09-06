2c654cacf201c425110b606e6dd4797bf98602cb
sunybee <suny_bee@163.com>
2020-09-06 20:58:56 +0800

    learn123

--- test_appium.py
+++ test_appium.py
@@ -6,3 +6,4 @@ desired_caps['deviceName']='emulator-5554'
 desired_caps['appPackage']='com.android.settings'
 desired_caps['appActivity']='com.android.settings.Settings'
 driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+#123
